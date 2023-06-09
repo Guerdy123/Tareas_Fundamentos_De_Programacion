4856af01926062f2f46179ebdce6901e64263319
Guerdy123 <96844218+Guerdy123@users.noreply.github.com>
2023-06-08 20:13:31 -0400

    Nodo

--- Nodo/main.c
+++ Nodo/main.c
@@ -2,12 +2,59 @@
 #include <stdlib.h>
 
 
-
+/* Structure nodo*/
 typedef struct nodo{
     int dato;
     struct nodo * siguiente;
 }Nodo;
 
+
+Nodo *crearNodo(int dato){
+    Nodo *n=(Nodo*) malloc(sizeof (Nodo));
+    n->dato=dato;
+    n->siguiente=NULL;
+    return n;
+}
+
+/*Structure Lista*/
+
+typedef struct {
+    Nodo *raiz;
+    int total;
+    Nodo *ultimo;
+}Lista;
+
+Lista  * crearLista(){
+    Lista * l = (Lista*) malloc(sizeof(Lista));
+    l->raiz=NULL;
+    l->total=0;
+    l->ultimo=NULL;
+    return l;
+}
+
+int estaVacia(Lista * l){  /*Verificar si lista esta vacia*/
+    if(l->raiz==NULL){
+        return 1;
+    } else{
+        return 0;
+    }
+}
+
+void insertarFinal( Lista * l,Nodo * n){
+    if (estaVacia(l)){ /*Si lista esta vacia*/
+        l->raiz=n;/*Primer elemento es igual a n*/
+        l->ultimo=n;/*ultimo elemento es igual a n*/
+    } else{
+        l->ultimo->siguiente=n;
+        l->ultimo=n;
+    }
+    l->total++;
+}
+
+
+void imprimirNodo(Nodo * n){
+    printf("%d\n",n->dato);
+}
 int main (){
 
 
