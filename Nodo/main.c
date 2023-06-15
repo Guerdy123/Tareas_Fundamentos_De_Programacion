3618672e8aa45cad1823301d035398552f1da60a
Guerdy123 <96844218+Guerdy123@users.noreply.github.com>
2023-06-15 14:11:32 -0400

    lista

--- Nodo/main.c
+++ Nodo/main.c
@@ -3,47 +3,60 @@
 
 
 /* Structure nodo*/
-typedef struct nodo{
+typedef struct nodo {
     int dato;
-    struct nodo * siguiente;
+    struct nodo *siguiente;
 }Nodo;
 
-
-Nodo *crearNodo(int dato){
-    Nodo *n=(Nodo*) malloc(sizeof (Nodo));
+Nodo  * crearNodo(int dato){
+    Nodo * n = (Nodo*) malloc(sizeof (Nodo));
     n->dato=dato;
     n->siguiente=NULL;
     return n;
 }
 
-/*Structure Lista*/
+void imprimirNodo(Nodo * n){
+    printf("%d\n",n->dato);
+}
 
-typedef struct {
-    Nodo *raiz;
+typedef struct lista{
+    Nodo * raiz;
     int total;
-    Nodo *ultimo;
+    Nodo * ultimo;
 }Lista;
 
-Lista  * crearLista(){
-    Lista * l = (Lista*) malloc(sizeof(Lista));
+
+Nodo * crearLista(){
+    Lista * l = (Lista*) malloc(sizeof (Lista));
     l->raiz=NULL;
     l->total=0;
     l->ultimo=NULL;
     return l;
-}
 
-int estaVacia(Lista * l){  /*Verificar si lista esta vacia*/
-    if(l->raiz==NULL){
-        return 1;
+}
+int estaVacia(Lista * l){
+    if((l->raiz=NULL)){
+       return 1;
     } else{
         return 0;
     }
 }
 
-void insertarFinal( Lista * l,Nodo * n){
-    if (estaVacia(l)){ /*Si lista esta vacia*/
-        l->raiz=n;/*Primer elemento es igual a n*/
-        l->ultimo=n;/*ultimo elemento es igual a n*/
+void insertarInicio(Nodo * n, Lista * l){
+    if (estaVacia(l)){
+        l->raiz = n;
+        l->ultimo=n;
+    } else{
+      n->siguiente=l->raiz;
+      l->raiz=n;
+    }
+    l->total++;
+}
+
+void insertarFinal(Nodo * n, Lista * l){
+    if (estaVacia(l)){
+        l->raiz = n;
+        l->ultimo=n;
     } else{
         l->ultimo->siguiente=n;
         l->ultimo=n;
@@ -51,10 +64,6 @@ void insertarFinal( Lista * l,Nodo * n){
     l->total++;
 }
 
-
-void imprimirNodo(Nodo * n){
-    printf("%d\n",n->dato);
-}
 int main (){
 
 
